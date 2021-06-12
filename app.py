77d0f58a6a6ad8e94b5a7244c5102b90dce72e72
may <83417030+may-gvg@users.noreply.github.com>
2021-06-12 10:57:47 +0200

    update app.py - import, csv, xstr,
    import poza funkcją, pobieranie danych z csv, select na komplet walut i pobranie wartości waluty , zmiana równania na zakup waluty obcej za pln, zmiana xstr na przelicz

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 
-from flask import request, redirect
-import requests
+from flask import request
 from flask import render_template
 import csv
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+def read_csv():
+    arr = {}
+    csvfile = open('output.csv', newline='', encoding="UTF-8")
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    for row in spamreader:
+        arr[row[1]] = row[3]
+    return arr
 
 
+try:
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data_from_json = response.json()
+    print(type(data_from_json))
+    csv_file = open("output.csv", 'w', encoding='utf-8', newline='')
+    writer = csv.writer(csv_file, delimiter=';')
+except:
+    print("Błąd")
+    exit(1)
 
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data_from_json = response.json()
-print(type(data_from_json))
-csv_file = open("output.csv", 'w', encoding='utf-8', newline='')
-writer = csv.writer(csv_file, delimiter=';')
 
 rates = data_from_json[0]['rates']
 for rate in rates:
@@ -30,29 +41,32 @@ csv_file.close()
 
 @app.route ('/waluty', methods=['GET', 'POST'])
 def waluty():
+   waluty = read_csv()
+   options = ""
+   for waluta in waluty:
+        options += '<option value="' + waluta + '">' + waluta + '</option>'
+   print(options)
+
+
    if request.method == 'GET':
        print("We received GET")
-       return render_template("waluty.html")
+       return render_template("waluty.html", options=options)
    elif request.method == 'POST':
        print("We received POST")
        print(request.form)
        currency = request.form.get('zczego')
        money = float(request.form.get('kwota'))
-       USD = 3.70
-       EUR = 4.51
-       CHF = 4.13
-       xstr = ""
+       waluty = read_csv()
+       for waluta in waluty:
+           if waluta == currency:
+               wartosc = float(waluty[waluta])
+       print("Wartosc: " + str(wartosc))
+       przelicz = ""
        print("Kwota: " + str(money))
        print("currenty: " + currency)
-       if currency == "USD":
-           xstr = str(round(money / USD, 3))
-       elif currency == "EUR":
-           xstr = str(round(money / EUR, 3))
-       elif currency == "CHF":
-           xstr = str(round(money / CHF, 3))
-       print("xstr: " + xstr)
+       przelicz = str(round(money * wartosc, 3))
+       print("przelicz: " + przelicz)
 
-       return render_template("waluty.html", wynik=xstr)
-
+       return render_template("waluty.html", wynik=przelicz, options=options)
 
 
